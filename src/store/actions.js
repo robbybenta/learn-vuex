e39a4f18cdfdab23fe93e84fe390ab7540e6b914
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 22:11:32 +0700

    fix: change arrow function to named function and add payload for getProduct

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,19 @@
 import axios from 'axios'
-export const getProducts = ({ commit }) => {
+export function getProducts({ commit }) {
   axios
     .get('http://shayna-backend.belajarkoding.com/api/products')
     .then(response => {
       commit('SET_PRODUCTS', response.data.data.data)
     })
 }
-export const getProduct = ({ commit }) => {
+export function getProduct({ commit }, { id }) {
   axios
     .get('http://shayna-backend.belajarkoding.com/api/products', {
       params: {
-        id: this.$route.params.id
+        id
       }
     })
     .then(response => {
       commit('SET_PRODUCT', response.data.data.data)
     })
-  console.log(this.id)
 }
